f2d89c8fcbcde9b9b78d0407762c35f4ebd8c456
Michelle <michey.asmah21@gmail.com>
2022-07-24 21:14:41 -0400

    fixed spring config

--- src/main/java/com/technews/TechNewsJavaApiApplication.java
+++ src/main/java/com/technews/TechNewsJavaApiApplication.java
@@ -2,10 +2,10 @@ package com.technews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class TechNewsJavaApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TechNewsJavaApiApplication.class, args);
 	}
